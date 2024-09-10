515ac1b9744e1670ef6ceba0c18eb2553c3e2a37
Bella Wunsch <bella.wunsch@mymail.champlain.edu>
2024-09-10 12:27:04 -0400

    Assign1_0.0004 : Cleaning up 1
    -Organised actions into regions to allow me  to add more information in each section as I learn

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -10,7 +10,7 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-//Triangle :3
+//Create Shape
 float vertices[] =
         {
                 -0.5f, -0.5f, 0.0f,
@@ -18,6 +18,7 @@ float vertices[] =
                 0.0f,  0.5f, 0.0f
         };
 
+    #pragma region Shader Sources
 const char *vertexShaderSource =
         R"(
             #version 330 core
@@ -40,47 +41,54 @@ const char *fragmentShaderSource =
             }
         )";
 
+#pragma endregion
+
 int main() {
-	printf("Initializing...");
-	if (!glfwInit()) {
-		printf("GLFW failed to init!");
-		return 1;
-	}
-	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
-	if (window == NULL) {
-		printf("GLFW failed to create window");
-		return 1;
-	}
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGL(glfwGetProcAddress)) {
-		printf("GLAD Failed to load GL headers");
-		return 1;
-	}
-	//Initialization goes here!
+    #pragma region Initialize Window
+    printf("Initializing...");
+    if (!glfwInit()) {
+        printf("GLFW failed to init!");
+        return 1;
+    }
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+    if (window == NULL) {
+        printf("GLFW failed to create window");
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        printf("GLAD Failed to load GL headers");
+        return 1;
+    }
+#pragma endregion
 
-    //Initializing Array Buffer
-
-        unsigned int VAO;
-        glGenVertexArrays(1, &VAO);
-        glBindVertexArray(VAO); //1
-
-        unsigned int VBO;
-        glGenBuffers(1, &VBO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        //Dynamic Draw is for every once in a while manipulation, Stream Draw is for every frame
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, 0); //clears buffer
-        //glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //2
+    #pragma region Create Vertex Array Object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0); //3
+#pragma endregion
 
+    #pragma region Create Vertex Buffer Object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //Dynamic Draw is for every once in a while manipulation, Stream Draw is for every frame
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // glBindBuffer(GL_ARRAY_BUFFER, 0); //clears buffer
+    //glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //2
+#pragma endregion
+
+    #pragma region Vertex Shader
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -97,13 +105,16 @@ int main() {
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
+#pragma endregion
 
+    #pragma region Fragment Shader
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+#pragma endregion
 
-
+    #pragma region Shader Program
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
@@ -119,6 +130,8 @@ int main() {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+#pragma endregion
+
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
@@ -127,13 +140,14 @@ int main() {
     //Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3); //todo : figure out what this does
 
 
         //Drawing happens here!
