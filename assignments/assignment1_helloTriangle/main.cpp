5650f8b5975805c81cf6839a3c8b5d6f62f11f2f
Bella Wunsch <alyx_wunsch@nyteshade.dev>
2024-09-10 10:52:08 -0400

    Assign1_0.0001 : Vertex Shader 1
    -Added Vertices float for triangle (not fully implemented)
    -Added Vertex Shader Source
    -Began Initilizing Array Biffer
    -Added Check for if shader fails

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -5,9 +5,29 @@
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <iostream>
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
+
+//Triangle :3
+float vertices[] =
+        {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f,  0.5f, 0.0f
+        };
+
+const char *vertexShaderSource =
+        R"(
+            #version 330 core
+            layout (location = 0) in vec3 aPos;
+
+            void main()
+            {
+               gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\
+            };
+        )";
 
 int main() {
 	printf("Initializing...");
@@ -26,8 +46,35 @@ int main() {
 		return 1;
 	}
 	//Initialization goes here!
-	
-	//Render loop
+
+    //Initializing Array Buffer
+        unsigned int VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        //Dynamic Draw is for every once in a while manipulation, Stream Draw is for every frame
+
+       // glBindBuffer(GL_ARRAY_BUFFER, 0); //clears buffer
+        //glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+
+    int  success;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    //Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		//Clear framebuffer
@@ -37,4 +84,5 @@ int main() {
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
+
 }
