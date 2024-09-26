3621ff867d528fcecbe5c0d1deb386c208566f17
Bella Wunsch <bella.wunsch@mymail.champlain.edu>
2024-09-26 17:13:36 -0400

    Assign2_0.0001 :
    -Created Directory for Assignment 2.
    **As a note, Assignment 2's CMakeLists.txt required the line
               project(assignment_2)
      to recognise a different directory**
    
    -Made a wavy square
    
    ==main.cpp==
    Variables :
    -Modified Vertices to account for fourth vertex
    -Added Indices variable
    
    Directionals :
    -Modified the steps to account for Element Buffer Object
    
    Render Loop :
    -Changed Draw call to DrawElements instead of DrawArrays

---
+++ assignments/assignment_2/main.cpp
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
+#include <Bella/shader.h>
+
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+
+int main() {
+
+    //1 : Create Program Window
+    #pragma region Initialize Window
+    printf("Initializing...");
+
+    //1(a) : Ensure Core Profile is Correct
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //1(b) : Check if GLFW fails to Initiate
+    if (!glfwInit()) {
+        printf("GLFW failed to init!");
+        return 1;
+    }
+
+    //1(c) : Create Window
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+    if (window == NULL) {
+        printf("GLFW failed to create window");
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        printf("GLAD Failed to load GL headers");
+        return 1;
+    }
+#pragma endregion
+
+    //2 : Instantiate Shader
+    Bella_GPR200::Shader ourShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+
+    //3 : Create Shape
+    float vertices[] =
+            {
+            //       X     Y     Z       R     G     B     A
+                    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, 1.0f,   // Bottom right
+                    0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f, 1.0f,   // Top right
+                    -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f,   // Bottom left
+                    -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f    // Top left
+            };
+
+    unsigned int indices[] =
+            {
+                0, 1, 2,    //Triangle 1
+                1, 2, 3     //Triangle 2
+            };
+
+    //4 : Instantiate Vertex Array Object, and Vertex Buffer Object
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    //5 : Initiate Element Buffer Object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    //6 : Bind Vertex Array Object
+    glBindVertexArray(VAO);
+
+    //7 : Bind Vertex Array Object
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //8 : Bind Element Buffer Object
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //9 : Attribute Pointer for Position (X,Y,Z)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //10 : Attribute Pointer for Colour (R,G,B,A)
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //11 : Render Loop
+    while (!glfwWindowShouldClose(window)) {
+
+        //11(a) : Clear the Screen
+        glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //11(b) : Instantiate Shader Uniforms
+        float timeValue = glfwGetTime();
+        ourShader.setFloat("uTime", timeValue);
+
+        //11(c) : Use Shader and Bind Vertex Array to Shader
+        ourShader.use();
+        glBindVertexArray(VAO);
+
+        //11(d) : Draw Call
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+        //11(e) : Swap Buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    //12 : Terminate Program
+    printf("Shutting down...");
+    glfwTerminate();
+
+}
