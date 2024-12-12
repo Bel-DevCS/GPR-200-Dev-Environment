fef90475e0814ce2e575331f43a3f3377353310d
AKonicki26 <anne.konicki@mymail.champlain.edu>
2024-12-12 14:31:17 -0500

    Model displays on scene

--- assignments/final_project/main.cpp
+++ assignments/final_project/main.cpp
@@ -4,15 +4,10 @@
 // Scene must be the first thing created, as it inits OpenGL from constructor
 final_project::Scene scene;
 
-
+Bella_GPR200::Shader genModelShader("./assets/genericModel.vert", "./assets/genericModel.frag");
+Bella_GPR200::Model testModel("./assets/Models/plant.fbx", genModelShader);
 
 int main() {
-
-    Bella_GPR200::Shader genModelShader("./assets/genericModel.vert", "./assets/genericModel.frag");
-    Bella_GPR200::Model testModel("./assets/Models/plant.fbx", genModelShader);
-
-
-
     scene.addModel(testModel);
 
     scene.play();
